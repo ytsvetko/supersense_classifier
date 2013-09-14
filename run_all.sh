a299d0992b8462248fd5b1d39ac0a032f0b210b4
ytsvetko <yulia.tsvetkov@gmail.com>
2013-09-14 15:52:35 -0400

    Two iteration expansion

--- run_all.sh
+++ run_all.sh
@@ -12,28 +12,62 @@ BROWN_CLUSTERS=data/brown/en-c600
 mkdir -p ${OUT_DIR}
 
 echo "Expand by WN synonyms and antonyms"
-if [ ! -f ${OUT_DIR}/train_expanded.txt ]; then
-  src/expand_labeled_data.py --labeled_data ${LABELS} \
-      --out_file ${OUT_DIR}/train_expanded.txt --expand 
-fi
+src/expand_labeled_data.py --labeled_data ${LABELS} \
+    --out_file ${OUT_DIR}/expanded.txt --expand 
 
-echo "Build training sets"
-if [ ! -f ${OUT_DIR}/train.feat ]; then
-src/build_training_sets.py --in_file ${OUT_DIR}/train_expanded.txt \
-    --out_dir ${OUT_DIR} --vocab ${VOCAB} --vectors ${WORD_VECTORS} \
+src/build_training_sets.py --in_file ${LABELS} \
+    --out_feat ${OUT_DIR}/train_seed.feat \
+    --out_labels ${OUT_DIR}/train_seed.labels \
+    --test_set ${OUT_DIR}/expanded.txt \
+    --out_test_feat ${OUT_DIR}/expanded.feat \
+    --out_test_labels ${OUT_DIR}/expanded.labels \
+    --vocab ${VOCAB} --vectors ${WORD_VECTORS} \
     --brown_clusters ${BROWN_CLUSTERS}
-fi
 
 echo "Run multi-way classifier. " #Default - Random Forest with 300 trees
-src/classify.py --train_features ${OUT_DIR}/train.feat \
-    --train_labels ${OUT_DIR}/train.labels \
-    --test_features ${OUT_DIR}/test.feat \
-    --test_predicted_labels_out ${OUT_DIR}/test.predicted \
+src/classify.py --train_features ${OUT_DIR}/train_seed.feat \
+    --train_labels ${OUT_DIR}/train_seed.labels \
+    --test_features ${OUT_DIR}/expanded.feat \
+    --golden_labels ${OUT_DIR}/expanded.labels \
+    --test_predicted_labels_out ${OUT_DIR}/expanded.predicted \
+    --priors "{\"RELATIONSHIP\":0.0}" \
     --write_posterior_probabilities \
     --num_cross_validation_folds 5 #--priors balanced --classifier "SVM"
 
 #${CREG_BIN} -x ${OUT_DIR}/train.feat -y ${OUT_DIR}/train.labels --l1 1.0 \
 #     --tx ${OUT_DIR}/test.feat -D -W > ${OUT_DIR}/test.predicted.creg
+
+
+## Iter 2
+
+echo "Selecting best expanded words"
+src/filter_expanded.py --predictions ${OUT_DIR}/expanded.predicted \
+    --orig_seed ${LABELS} \
+    --out_file ${OUT_DIR}/expanded_seed.txt
+
+echo "Expand by WN synonyms and antonyms"
+src/expand_labeled_data.py --labeled_data ${OUT_DIR}/expanded_seed.txt \
+    --out_file ${OUT_DIR}/expanded.txt --expand 
+
+src/build_training_sets.py --in_file ${OUT_DIR}/expanded_seed.txt \
+    --out_feat ${OUT_DIR}/train_seed.feat \
+    --out_labels ${OUT_DIR}/train_seed.labels \
+    --test_set ${OUT_DIR}/expanded.txt \
+    --out_test_feat ${OUT_DIR}/expanded.feat \
+    --out_test_labels ${OUT_DIR}/expanded.labels \
+    --vocab ${VOCAB} --vectors ${WORD_VECTORS} \
+    --brown_clusters ${BROWN_CLUSTERS}
+
+echo "Run multi-way classifier. " #Default - Random Forest with 300 trees
+src/classify.py --train_features ${OUT_DIR}/train_seed.feat \
+    --train_labels ${OUT_DIR}/train_seed.labels \
+    --test_features ${OUT_DIR}/expanded.feat \
+    --golden_labels ${OUT_DIR}/expanded.labels \
+    --test_predicted_labels_out ${OUT_DIR}/expanded.predicted \
+    --priors "{\"RELATIONSHIP\":0.0}" \
+    --write_posterior_probabilities \
+    --num_cross_validation_folds 5 #--priors balanced --classifier "SVM"
+
 
 #TODO Active learning
 echo "Update training sets"
--- src/build_training_sets.py
+++ src/build_training_sets.py
@@ -10,8 +10,12 @@ import json
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--in_file", required=True)
-parser.add_argument("--out_dir", required=True)
+parser.add_argument("--out_feat", required=True)
+parser.add_argument("--out_labels", required=True)
 
+parser.add_argument("--test_set")
+parser.add_argument("--out_test_feat")
+parser.add_argument("--out_test_labels")
 parser.add_argument("--vocab")
 parser.add_argument("--vectors")
 parser.add_argument("--brown_clusters")
@@ -41,24 +45,42 @@ def main(argv):
   vectors = LoadVectors(args.vocab, args.vectors)
   LoadBrownClusters(args.brown_clusters, vectors)
 
-  out_features = open(os.path.join(args.out_dir, "train.feat"), "w")
-  out_labels = open(os.path.join(args.out_dir, "train.labels"), "w")
+  out_features = open(args.out_feat, "w")
+  out_labels = open(args.out_labels, "w")
   seen_in_training = set()
   for line in open(args.in_file):
-    word, label, relation = line.strip().split("\t")
+    tokens = line.strip().split("\t")
+    if len(tokens) == 1:
+      continue
+    if len(tokens) == 2:
+      tokens.append("seed")
+    word, label, relation = tokens
     if word not in vectors:
       continue
     seen_in_training.add(word)
     features_str = json.dumps(vectors[word], sort_keys=True)
     out_features.write("{}_{}_{}\t{}\n".format(word, label, relation, features_str))
     out_labels.write("{}_{}_{}\t{}\n".format(word, label, relation, label.upper()))
-  test_features_file = open(os.path.join(args.out_dir, "test.feat"), "w")
-  for word, features in sorted(vectors.iteritems()):
-    if word in seen_in_training:
-      continue
-    features_str = json.dumps(features, sort_keys=True)
-    test_features_file.write("{}_vocab\t{}\n".format(word, features_str))
 
+  if args.out_test_feat:
+    test_features_file = open(args.out_test_feat, "w")
+    if args.test_set is None:
+      for word, features in sorted(vectors.iteritems()):
+        if word in seen_in_training:
+          continue
+        features_str = json.dumps(features, sort_keys=True)
+        test_features_file.write("{}_vocab\t{}\n".format(word, features_str))
+    else:
+      test_labels_file = open(args.out_test_labels, "w")
+      for line in open(args.test_set):
+        word, label, relation = line.strip().split("\t")
+        if word not in vectors:
+          continue
+        if word in seen_in_training:
+          continue
+        features_str = json.dumps(vectors[word], sort_keys=True)
+        test_features_file.write("{}_{}_{}\t{}\n".format(word, label, relation, features_str))
+        test_labels_file.write("{}_{}_{}\t{}\n".format(word, label, relation, label.upper()))
 
 if __name__ == '__main__':
   main(sys.argv)
